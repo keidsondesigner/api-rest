5c6e0255143cc3395eefbfb07d4e4f898a5947a4
keidsondesigner <keidsondesigner@gmail.com>
2022-11-25 00:00:24 -0400

    ajustes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
 	console.log("Server exucutando na porta http://localhost:5000")
 })
